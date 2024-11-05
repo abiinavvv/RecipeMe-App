4178f0fc0841ef1978c9f0cac06f43ab25f39a60
abiinavvv <abhinavaneesh2@gmail.com>
2024-11-05 11:45:57 +0530

    settings update

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,58 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import '../models/app_state.dart';
-// import 'privacy_policy_screen.dart'; 
-
-// class SettingScreen extends StatelessWidget {
-//   const SettingScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Settings'),
-//       ),
-//       body: Consumer<AppState>(
-//         builder: (context, appState, child) {
-//           return ListView(
-//             children: [
-//               ListTile(
-//                 title: const Text('Dark Mode'),
-//                 trailing: Switch(
-//                   value: appState.isDarkMode,
-//                   onChanged: (bool value) {
-//                     appState.toggleDarkMode();
-//                   },
-//                 ),
-//               ),
-//               ListTile(
-//                 title: const Text('Privacy Policy'),
-//                 onTap: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                       builder: (context) => const PrivacyPolicyScreen(),
-//                     ),
-//                   );
-//                 },
-//               ),
-//               const ListTile(
-//                 title: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Text('Version'),
-
-//                     Text('1.0.0'), // version
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/app_state.dart';
@@ -108,42 +53,9 @@ class SettingScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text('Version'),
-                    Text('1.0.0'), // version
+                    Text('1.1.0'), // version
                   ],
                 ),
-              ),
-              ListTile(
-                title: const Text('Logout'),
-                trailing: const Icon(Icons.logout),
-                onTap: () {
-                  // Show a confirmation dialog
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Logout'),
-                        content: const Text('Are you sure you want to logout?'),
-                        actions: <Widget>[
-                          TextButton(
-                            child: const Text('Cancel'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          TextButton(
-                            child: const Text('Logout'),
-                            onPressed: () {
-                              // Perform logout action
-                              //appState.logout();
-                              // Navigate to login screen
-                              Navigator.of(context).pushReplacementNamed('/login');
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
               ),
             ],
           );
